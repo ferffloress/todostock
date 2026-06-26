340e0c461b6000e442b1c7f2ec64fd4918f45bb6
fernanda <fernanda.psievidencia@gmail.com>
2026-06-26 13:38:13 -0300

    descuento por bulto

---
+++ src/utils/precios.js
@@ -0,0 +1,18 @@
+function calcularPrecioFacturado(tipoVenta, precioUnitario) {
+  const precioBase = Number(precioUnitario) || 0;
+  if (tipoVenta === "bulto") {
+    return Number((precioBase * 0.8).toFixed(2));
+  }
+  return Number(precioBase.toFixed(2));
+}
+
+function calcularSubtotalItem({ tipoVenta, cantidad, precioUnitario }) {
+  const cantidadFinal = Number(cantidad) || 0;
+  const precioFacturado = calcularPrecioFacturado(tipoVenta, precioUnitario);
+  return Number((cantidadFinal * precioFacturado).toFixed(2));
+}
+
+module.exports = {
+  calcularPrecioFacturado,
+  calcularSubtotalItem,
+};
